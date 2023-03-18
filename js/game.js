d80feb6b57271d16374e52c70b21360ac7337b3c
AngelesVFA <spotifyasuna21@gmail.com>
2023-03-18 10:33:23 +0100

    collision detected

--- js/game.js
+++ js/game.js
@@ -1,46 +1,43 @@
 class Game {
   constructor(ctx, canvasHeight, canvasWidth) {
-    this.ctx = ctx,
-      this.canvasHeight = canvasHeight,
-      this.canvasWidth = canvasWidth
-      this.info = [this.ctx, this.canvasHeight, this.canvasWidth];
-      this.background = undefined
-      this.zombies = []
-      this.frameCounter = 0
-      this.init()
+    (this.ctx = ctx),
+      (this.canvasHeight = canvasHeight),
+      (this.canvasWidth = canvasWidth);
+    this.info = [this.ctx, this.canvasHeight, this.canvasWidth];
+    this.background = undefined;
+    this.zombies = [];
+    this.frameCounter = 0;
+    this.init();
   }
-  init(){
+  init() {
     this.background = new Background(...this.info);
-    this.player = new Player(...this.info)
-    this.createZombie()
+    this.player = new Player(...this.info);
+    this.createZombie();
   }
-  draw(){
-    ++ this.frameCounter
-    this.background.draw()
-    this.player.draw()
-    if(this.frameCounter === 180){
-      this.createZombie()
-      this.frameCounter = 0
+  draw() {
+    ++this.frameCounter;
+    this.background.draw();
+    this.player.draw();
+    if (this.frameCounter === 180) {
+      this.createZombie();
+      this.frameCounter = 0;
     }
-    this.isColliding(this.zombies, this.player)
-   this.zombies.forEach((zombie)=>{
-    zombie.draw()
-   })
+    this.zombies.forEach((zombie) => {
+      this.isColliding(zombie, this.player);
+      zombie.draw();
+    });
   }
-  createZombie(){
-     this.zombies.push(new Zombie(...this.info))
+  createZombie() {
+    this.zombies.push(new Zombie(...this.info));
   }
-  isColliding(obj1, obj2){
+  isColliding(obj1, obj2) {
     if (
       obj1.posXY.x < obj2.posXY.x + obj2.size.w &&
       obj1.posXY.x + obj1.size.w > obj2.posXY.x &&
       obj1.posXY.y < obj2.posXY.y + obj2.size.h &&
       obj1.posXY.x + obj1.posXY.y > obj2.posXY.y
     ) {
-      console.log('está colisionando')
-      
+      window.alert("Está colisionando");
     }
   }
-  
-
 }
